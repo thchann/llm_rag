29a055da72ac5b37687b1c507aa4dc66aae28979
Theodore Chan <theodorechan@mac.trinity.local>
2025-05-09 23:30:09 -0500

    commit

--- visualize_chunks.py
+++ visualize_chunks.py
@@ -6,7 +6,6 @@ np.random.seed(42)
 chunk_sizes = np.random.normal(loc=490, scale=10, size=10000)
 chunk_sizes = np.clip(chunk_sizes, 250, 500)
 
-# Plot
 plt.figure(figsize=(10, 6))
 sns.histplot(chunk_sizes, bins=20, kde=True, color="orange", edgecolor="black", alpha=0.4)
 plt.title("Estimated Distribution of Chunk Sizes", fontsize=14)
